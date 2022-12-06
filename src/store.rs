e2230c8382d3944eb46fa72522bcbc8611e0089e
Timo <node1@colomba.link>
2022-12-06 11:34:04 +0100

    Reinserted Store Trait, which was removed in v15

--- src/store.rs
+++ src/store.rs
@@ -16,8 +16,13 @@
 //! However it is intended to run only when the configured size is exceeded at when it will start
 //! incrementally deleting unaliased blocks until the size target is no longer exceeded. It is
 //! implementation defined in which order unaliased blocks get removed.
+use libipld_core::codec::Decode;
+use libipld_core::ipld::Ipld;
+use crate::{Block, Cid, DagPath};
 use crate::codec::Codec;
 use crate::multihash::MultihashDigest;
+use crate::error::Result;
+use async_trait::async_trait;
 
 /// The store parameters.
 pub trait StoreParams: std::fmt::Debug + Clone + Send + Sync + Unpin + 'static {
@@ -37,4 +42,70 @@ impl StoreParams for DefaultParams {
     const MAX_BLOCK_SIZE: usize = 1_048_576;
     type Codecs = crate::IpldCodec;
     type Hashes = crate::multihash::Code;
+}
+
+/// Implementable by ipld stores. An ipld store behaves like a cache. It will keep blocks
+/// until the cache is full after which it evicts blocks based on an eviction policy. If
+/// a block is aliased (recursive named pin), it and it's recursive references will not
+/// be evicted or counted towards the cache size.
+#[async_trait]
+pub trait Store: Clone + Send + Sync {
+    /// Store parameters.
+    type Params: StoreParams;
+    /// Temp pin.
+    type TempPin: Clone + Send + Sync;
+
+    /// Creates a new temporary pin.
+    fn create_temp_pin(&self) -> Result<Self::TempPin>;
+
+    /// Adds a block to a temp pin.
+    fn temp_pin(&self, tmp: &Self::TempPin, cid: &Cid) -> Result<()>;
+
+    /// Returns true if the store contains the block.
+    fn contains(&self, cid: &Cid) -> Result<bool>;
+
+    /// Returns a block from the store. If the block wasn't found it returns a `BlockNotFound`
+    /// error.
+    fn get(&self, cid: &Cid) -> Result<Block<Self::Params>>;
+
+    /// Inserts a block into the store and publishes the block on the network.
+    fn insert(&self, block: &Block<Self::Params>) -> Result<()>;
+
+    /// Creates an alias for a `Cid`.
+    fn alias<T: AsRef<[u8]> + Send + Sync>(&self, alias: T, cid: Option<&Cid>) -> Result<()>;
+
+    /// Resolves an alias for a `Cid`.
+    fn resolve<T: AsRef<[u8]> + Send + Sync>(&self, alias: T) -> Result<Option<Cid>>;
+
+    /// Returns all the aliases that are keeping the block around.
+    fn reverse_alias(&self, cid: &Cid) -> Result<Option<Vec<Vec<u8>>>>;
+
+    /// Flushes the store.
+    async fn flush(&self) -> Result<()>;
+
+    /// Returns a block from the store. If the store supports networking and the block is not
+    /// in the store it fetches it from the network and inserts it into the store. Dropping the
+    /// future cancels the request.
+    ///
+    /// If the block wasn't found it returns a `BlockNotFound` error.
+    async fn fetch(&self, cid: &Cid) -> Result<Block<Self::Params>>;
+
+    /// Fetches all missing blocks recursively from the network. If a block isn't found it
+    /// returns a `BlockNotFound` error.
+    async fn sync(&self, cid: &Cid) -> Result<()>;
+
+    /// Resolves a path recursively and returns the ipld.
+    async fn query(&self, path: &DagPath<'_>) -> Result<Ipld>
+        where
+            Ipld: Decode<<Self::Params as StoreParams>::Codecs>,
+    {
+        let mut ipld = self.fetch(path.root()).await?.ipld()?;
+        for segment in path.path().iter() {
+            ipld = ipld.take(segment)?;
+            if let Ipld::Link(cid) = ipld {
+                ipld = self.fetch(&cid).await?.ipld()?;
+            }
+        }
+        Ok(ipld)
+    }
 }
